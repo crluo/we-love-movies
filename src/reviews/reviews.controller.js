287e013aa6488a72ba8aa01fccbd0346312797b8
Crystal Luo <crystalluo@Crystals-MacBook-Pro.local>
2021-10-20 11:59:56 -0400

    added commments and code clean up

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -1,5 +1,10 @@
 const service = require("./reviews.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
+/**
+ * verifies review id or responds with error if not found
+ * 
+ */
 async function reviewExists(req, res, next) {
     const reviewId = req.params.reviewId;
     const foundReview = await service.read(reviewId);
@@ -14,6 +19,9 @@ async function reviewExists(req, res, next) {
     next();
 }
 
+/**
+ * updates an existing review and responds with the updated review 
+ */
 async function update(req, res, next) {
     let updatedReview = {
         ...res.locals.review,
@@ -23,13 +31,15 @@ async function update(req, res, next) {
     updatedReview = await service.updateReviewWithCritic(res.locals.reviewId);
     res.json({ data: updatedReview });
 }
-
+/**
+ * deletes an existing review and responds with 204 status on completion
+ */
 async function destroy(req, res, next) {
     await service.destroy(res.locals.reviewId);
     res.sendStatus(204);
 }
 
 module.exports = {
-    update: [ reviewExists, update ],
-    destroy: [ reviewExists, destroy ],
+    update: [ asyncErrorBoundary(reviewExists), update ],
+    destroy: [ asyncErrorBoundary(reviewExists), destroy ],
 }
